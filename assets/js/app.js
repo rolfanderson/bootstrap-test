a9738b36132026496a1986b823258b363c4a6b32
Rolf Anderson <rolf.k.anderson@gmail.com>
2017-05-25 16:17:41 -0400

    bootleaf

--- assets/js/app.js
+++ assets/js/app.js
@@ -68,11 +68,11 @@ function animateSidebar() {
     map.invalidateSize();
   });
 }
-
+/*
 function sizeLayerControl() {
   $(".leaflet-control-layers").css("max-height", $("#map").height() - 50);
 }
-
+*/
 function clearHighlight() {
   highlight.clearLayers();
 }
@@ -121,11 +121,12 @@ var disney = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
   maxZoom: 19,
   attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 });
-
+/*
 var cartoLight = L.tileLayer("https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png", {
   maxZoom: 19,
   attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="https://cartodb.com/attributions">CartoDB</a>'
 });
+
 var usgsImagery = L.layerGroup([L.tileLayer("http://basemap.nationalmap.gov/arcgis/rest/services/USGSImageryOnly/MapServer/tile/{z}/{y}/{x}", {
   maxZoom: 15,
 }), L.tileLayer.wms("http://raster.nationalmap.gov/arcgis/services/Orthoimagery/USGS_EROS_Ortho_SCALE/ImageServer/WMSServer?", {
@@ -136,8 +137,9 @@ var usgsImagery = L.layerGroup([L.tileLayer("http://basemap.nationalmap.gov/arcg
   transparent: true,
   attribution: "Aerial Imagery courtesy USGS"
 })]);
-
+*/
 /* Overlay Layers */
+/*
 var highlight = L.geoJson(null);
 var highlightStyle = {
   stroke: false,
@@ -217,17 +219,18 @@ var subwayLines = L.geoJson(null, {
 $.getJSON("data/subways.geojson", function (data) {
   subwayLines.addData(data);
 });
-
+*/
 /* Single marker cluster layer to hold all clusters */
-
+/*
 var markerClusters = new L.MarkerClusterGroup({
   spiderfyOnMaxZoom: true,
   showCoverageOnHover: false,
   zoomToBoundsOnClick: true,
   disableClusteringAtZoom: 16
 });
-
+*/
 /* Empty layer placeholder to add to layer control for listening when to add/remove theaters to markerClusters layer */
+
 var theaterLayer = L.geoJson(null);
 var theaters = L.geoJson(null, {
   pointToLayer: function (feature, latlng) {
@@ -322,7 +325,78 @@ map = L.map("map", {
   attributionControl: false
 });
 
+var ripIcon = L.icon({
+              iconUrl: 'http://i.imgur.com/TaHF1B8.png',
+              iconSize: [25, 25],
+});
+
+var marker
+
+var markers = [
+              //Magic Kingdom
+              { coords: [28.418395, -81.578169], uri: 'https://tinyurl.com/k9dvj5u' },
+              //skyway, 15 February 1999
+              { coords: [28.419296, -81.580979], uri: 'https://tinyurl.com/mlnjcn7' },
+              //spaceMountain, 12 August 1980
+              { coords: [28.419192, -81.577707], uri: 'https://tinyurl.com/ld67pk3' },
+              //spaceMountain, 1 August 2006
+              { coords: [28.418824, -81.577455], uri: 'https://tinyurl.com/n7dx4a6' },
+              //spaceMountain, 7 December 2006
+              { coords: [28.418786, -81.577202], uri: 'https://tinyurl.com/lxqh5fb' },
+              //spaceMountain, 7 July 2015
+              { coords: [28.417909, -81.584142], uri: 'https://tinyurl.com/jwg8mr3' },
+              //piratesCaribbean, February 2005
+              { coords: [28.417958, -81.584346], uri: 'https://tinyurl.com/leo4bc9' },
+              //piratesCaribbean, 6 August 2009
+              { coords: [28.418962, -81.577671], uri:'https://tinyurl.com/mvzyddt' },
+              //cinderellaCastle, 11 August 1977
+              { coords: [28.420159, -81.581194], uri: 'https://tinyurl.com/knob9jl' },
+              //carrousel, 12 December 2010
+              { coords: [28.420490, -81.582037], uri: 'https://tinyurl.com/k9zcogs' },
+              //smallWorld, 25 December 2014
+              { coords: [28.419169, -81.585052], uri: 'https://tinyurl.com/mzuwncm' },
+              //splashMountain, 6 November 2000
+              { coords: [28.417928, -81.583496], uri: 'https://tinyurl.com/n8nna9v' },
+              //jungleCruise, 23 March 2012, died 7 April 2012
+              
+              //Epcot
+              { coords: [28.373862, -81.552383], uri: 'https://tinyurl.com/mycq7sv' },
+              //soarin', 15 January 2007
+              { coords: [28.373929, -81.546975], uri: 'https://tinyurl.com/mycq7sv' },
+              //missionSpace, 11 April 2006
+              //Hollywood Studios
+              { coords: [28.359702, -81.560612], uri: 'https://tinyurl.com/mycq7sv' },
+              //rocknRollerCoaster, 29 June 2016
+              { coords: [28.35619, -81.561052], uri: 'https://tinyurl.com/mycq7sv' },
+              //toyStory, 4 October 2014
+              { coords: [28.35563, -81.55879], uri: 'https://tinyurl.com/mycq7sv' },
+              //starTours, 11 July 2011
+              //Animal Kingdom
+              { coords: [28.35866, -81.587294], uri: 'https://tinyurl.com/mycq7sv' },
+              //expeditionEverest, 18 December 2007
+              //Water Parks
+              { coords: [28.36586, -81.52973], uri: 'https://tinyurl.com/mycq7sv' },
+              //surfPool, typhoonLagoon, 26 August 2012
+              { coords: [28.350997, -81.575123], uri: 'https://tinyurl.com/mycq7sv' },
+              //downhillDoubleDipper, 16 March 2007
+              //Resorts
+              { coords: [28.415069, -81.574430], uri: 'https://tinyurl.com/z2xza9k' },
+              //contemporary, 22 March 2016
+              { coords: [28.410341, -81.587041], uri: 'https://tinyurl.com/jjyfjnb' },
+      ];
+
+      var x = markers.length;
+
+      while(x--) {
+            L.marker(markers[x].coords, {icon: ripIcon}).on('click', function(e) {
+            window.location = markers[e.target._leaflet_id].uri;
+            }).addTo(map)._leaflet_id = x;
+      }
+
+
+
 /* Layer control listeners that allow for a single markerClusters layer */
+/*
 map.on("overlayadd", function(e) {
   if (e.layer === theaterLayer) {
     markerClusters.addLayer(theaters);
@@ -344,7 +418,7 @@ map.on("overlayremove", function(e) {
     syncSidebar();
   }
 });
-
+*/
 /* Filter sidebar feature list to only show features in current map bounds */
 map.on("moveend", function (e) {
   syncSidebar();
@@ -442,11 +516,13 @@ var layerControl = L.control.groupedLayers(baseLayers, groupedOverlays, {
 */
 
 /* Highlight search box text on click */
+/*
 $("#searchbox").click(function () {
   $(this).select();
 });
-
+*/
 /* Prevent hitting enter from refreshing the page */
+/*
 $("#searchbox").keypress(function (e) {
   if (e.which == 13) {
     e.preventDefault();
@@ -456,12 +532,15 @@ $("#searchbox").keypress(function (e) {
 $("#featureModal").on("hidden.bs.modal", function (e) {
   $(document).on("mouseout", ".feature-row", clearHighlight);
 });
+*/
 
 /* Typeahead search functionality */
+/*
 $(document).one("ajaxStop", function () {
   $("#loading").hide();
-  sizeLayerControl();
+  sizeLayerControl(); */
   /* Fit map to boroughs bounds */
+  /*
   map.fitBounds(boroughs.getBounds());
   featureList = new List("features", {valueNames: ["feature-name"]});
   featureList.sort("feature-name", {order:"asc"});
@@ -530,8 +609,10 @@ $(document).one("ajaxStop", function () {
   theatersBH.initialize();
   museumsBH.initialize();
   geonamesBH.initialize();
+  */
 
   /* instantiate the typeahead UI */
+  /*
   $("#searchbox").typeahead({
     minLength: 3,
     highlight: true,
@@ -604,6 +685,7 @@ $(document).one("ajaxStop", function () {
   $(".twitter-typeahead").css("position", "static");
   $(".twitter-typeahead").css("display", "block");
 });
+*/
 
 // Leaflet patch to make layer control scrollable on touch browsers
 var container = $(".leaflet-control-layers")[0];
