9bd7d17a8f88953d1b80f19d24ebe3a638a3cb9a
Rolf Anderson <rolf.k.anderson@gmail.com>
2017-05-25 15:35:46 -0400

    bootleaf

---
+++ assets/js/map.js
@@ -0,0 +1,102 @@
+/*Here's all the custom stuff for the map*/
+  var map = L.map('map',{scrollWheelZoom:false}).setView([28.4187304,-81.581206], 17);
+
+            L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+            maxZoom: 19,
+            attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(map);
+
+  var ripIcon = L.icon({
+                iconUrl: 'http://i.imgur.com/TaHF1B8.png',
+                iconSize: [25, 25],
+  });
+
+  var marker
+
+  var markers = [
+                //Magic Kingdom
+                { coords: [28.418395, -81.578169], uri: 'https://tinyurl.com/k9dvj5u' },
+                //skyway, 15 February 1999
+                { coords: [28.419296, -81.580979], uri: 'https://tinyurl.com/mlnjcn7' },
+                //spaceMountain, 12 August 1980
+                { coords: [28.419192, -81.577707], uri: 'https://tinyurl.com/ld67pk3' },
+                //spaceMountain, 1 August 2006
+                { coords: [28.418824, -81.577455], uri: 'https://tinyurl.com/n7dx4a6' },
+                //spaceMountain, 7 December 2006
+                { coords: [28.418786, -81.577202], uri: 'https://tinyurl.com/lxqh5fb' },
+                //spaceMountain, 7 July 2015
+                { coords: [28.417909, -81.584142], uri: 'https://tinyurl.com/jwg8mr3' },
+                //piratesCaribbean, February 2005
+                { coords: [28.417958, -81.584346], uri: 'https://tinyurl.com/leo4bc9' },
+                //piratesCaribbean, 6 August 2009
+                { coords: [28.418962, -81.577671], uri:'https://tinyurl.com/mvzyddt' },
+                //cinderellaCastle, 11 August 1977
+                { coords: [28.420159, -81.581194], uri: 'https://tinyurl.com/knob9jl' },
+                //carrousel, 12 December 2010
+                { coords: [28.420490, -81.582037], uri: 'https://tinyurl.com/k9zcogs' },
+                //smallWorld, 25 December 2014
+                { coords: [28.419169, -81.585052], uri: 'https://tinyurl.com/mzuwncm' },
+                //splashMountain, 6 November 2000
+                { coords: [28.417928, -81.583496], uri: 'https://tinyurl.com/n8nna9v' },
+                //jungleCruise, 23 March 2012, died 7 April 2012
+                { coords: [28.420450, -81.583123], uri: 'https://rolfanderson.github.io/projectmortimer/mortimer.html'},
+                //hauntedMansion (not a death)
+                //Epcot
+                { coords: [28.373862, -81.552383], uri: 'https://tinyurl.com/mycq7sv' },
+                //soarin', 15 January 2007
+                { coords: [28.373929, -81.546975], uri: 'https://tinyurl.com/mycq7sv' },
+                //missionSpace, 11 April 2006
+                //Hollywood Studios
+                { coords: [28.359702, -81.560612], uri: 'https://tinyurl.com/mycq7sv' },
+                //rocknRollerCoaster, 29 June 2016
+                { coords: [28.35619, -81.561052], uri: 'https://tinyurl.com/mycq7sv' },
+                //toyStory, 4 October 2014
+                { coords: [28.35563, -81.55879], uri: 'https://tinyurl.com/mycq7sv' },
+                //starTours, 11 July 2011
+                //Animal Kingdom
+                { coords: [28.35866, -81.587294], uri: 'https://tinyurl.com/mycq7sv' },
+                //expeditionEverest, 18 December 2007
+                //Water Parks
+                { coords: [28.36586, -81.52973], uri: 'https://tinyurl.com/mycq7sv' },
+                //surfPool, typhoonLagoon, 26 August 2012
+                { coords: [28.350997, -81.575123], uri: 'https://tinyurl.com/mycq7sv' },
+                //downhillDoubleDipper, 16 March 2007
+                //Resorts
+                { coords: [28.415069, -81.574430], uri: 'https://tinyurl.com/z2xza9k' },
+                //contemporary, 22 March 2016
+                { coords: [28.410341, -81.587041], uri: 'https://tinyurl.com/jjyfjnb' },
+        ];
+
+        var x = markers.length;
+
+        while(x--) {
+              L.marker(markers[x].coords, {icon: ripIcon}).on('click', function(e) {
+              window.location = markers[e.target._leaflet_id].uri;
+              }).addTo(map)._leaflet_id = x;
+        }
+
+        L.easyButton( '<i class="mdi mdi-help mdi-18px"></i>', function(){
+              alert('Search the map to reveal abject sacrifices.');
+        }).addTo(map);
+
+      /*
+        L.easyButton('<i class="mdi mdi-auto-fix mdi-18px"></i>', function(btn, map){
+              var magicKingdom = [28.4187304, -81.581206];
+              map.setView(magicKingdom);
+        }).addTo(map);
+
+        L.easyButton('<i class="material-icons">language</i>', function(btn, map){
+              var epcot = [28.37324, -81.54939];
+              map.setView(epcot);
+        }).addTo(map);
+
+        L.easyButton('<i class="material-icons">movie_filter</i>', function(btn, map){
+              var hollywoodStudios = [28.35757, -81.55982];
+              map.setView(hollywoodStudios);
+        }).addTo(map);
+
+        L.easyButton('<i class="material-icons">bug_report</i>', function(btn, map){
+              var animalKingdom = [28.36053, -81.59161];
+              map.setView(animalKingdom);
+        }).addTo(map);
+      */
