018ba0bec8905743c2c87c46858af2895ab83d78
Rolf Anderson <rolf.k.anderson@gmail.com>
2017-06-04 08:35:58 -0400

    update

--- js/map.js
+++ js/map.js
@@ -45,7 +45,7 @@ function sidebarClick(id) {
 
 var disney = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   maxZoom: 19,
-  minZoom: 14,
+  minZoom: 15,
   attribution: '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 });
 
