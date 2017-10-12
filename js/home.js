ca9a09980fb3b04e7209c8dd0c03ae6f2dda6ba2
Rolf Anderson <rolf.k.anderson@gmail.com>
2017-10-12 18:54:42 -0400

    update

--- js/home.js
+++ js/home.js
@@ -32,9 +32,49 @@ function sidebarClose() {
     return false;
 }
 
-var cards = $(".random-section");
+$("#sidebar-toggle-btn").click(function() {
+    var $nodes = $("#Items").find("li");
+    shuffle($nodes, "sw");
+    $("#Items").append($nodes);
+});
+
+
+//Function that will shuffle only your switchable elements.
+function shuffle(nodes, switchableSelector) {
+    var length = nodes.length;
+
+    //Create the array for the random pick.
+    var switchable = nodes.filter("." + switchableSelector);
+    var switchIndex = [];
+
+    $.each(switchable, function(index, item) {
+       switchIndex[index] = $(item).index();
+    });
+
+    //The array should be used for picking up random elements.
+    var switchLength = switchIndex.length;
+    var randomPick, randomSwap;
+
+    for (var index = length; index > 0; index--) {
+        //Get a random index that contains a switchable element.
+        randomPick = switchIndex[Math.floor(Math.random() * switchLength)];
+
+        //Get the next element that needs to be swapped.
+        randomSwap = nodes[index - 1];
+
+        //If the element is 'not switchable', ignore and continue;
+        if($(randomSwap).hasClass(switchableSelector)) {
+            nodes[index - 1] = nodes[randomPick];
+            nodes[randomPick] = randomSwap;
+        }
+    }
+
+    return nodes;
+}
+
+/*var cards = $(".random-section");
 for(var i = 0; i < cards.length; i++){
     var target = Math.floor(Math.random() * cards.length -1) + 1;
     var target2 = Math.floor(Math.random() * cards.length -1) +1;
     cards.eq(target).before(cards.eq(target2));
-}
+}*/
